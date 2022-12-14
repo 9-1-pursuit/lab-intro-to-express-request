21b5a5258da767e29ae161b324e63f462ae9181c
bowersjuan <bowersjuan@gmail.com>
2022-12-14 17:39:29 -0500

    Update app.js and server.js for commonjs imports and add routes to app.js

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
-import express from "express";
+const express = require("express");
 
 const app = express();
 
+const bugsResponse = `<p>99 little bugs in the code</p>
+    <a href={\"\"} target=\"blank\">
+      pull one down, patch it around
+    </a>`;
 app.get("/", (req, res) => {
-  res.send("App is running...");
+  res.send("Welcome 99 Pokemon");
 });
 
-export default app;
+app.get("/bugs", (req, res) => {
+  res.send(bugsResponse);
+});
+
+app.get("/:verb/:adj/:noun", (req, res) => {
+  const { verb, adj, noun } = req.params;
+
+  res.send(
+    "Congratulations on starting a new project called " +
+      `${verb}-${adj}-${noun}!`
+  );
+});
+
+module.exports = app;
