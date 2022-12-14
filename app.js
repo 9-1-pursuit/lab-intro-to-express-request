3d7993fe67c133f5481e611eb5405be99cc0bba5
TonyH98 <tonyhoang@pursuit.org>
2022-12-14 14:27:44 -0500

    9 out 11 tests are passing

---
+++ app.js
@@ -0,0 +1,63 @@
+const express = require('express')
+const pokemon = require('./models/pokemon.json')
+const app = express()
+
+
+app.get("/", (req, res) => {
+    res.send("Welcome 99 Pokemon")
+})
+
+app.get("/:verb/:adjective/:noun", (req, res) => {
+    const {verb , adjective , noun} = req.params
+    res.send(`Congratulations on starting a new project called ${verb}-${adjective}-${noun}!`)
+})
+
+
+
+app.get("/bugs", (req , res) => {
+    res.send("<h1>99 little bugs in the code</h1>")
+})
+
+app.get("/bugs/:numberofBugs", (req , res) => {
+    const {numberofBugs} = req.params
+    if(numberofBugs >= 200){
+        res.send("Too many bugs!! Start over!")
+    }
+    else{
+        res.send(`${numberofBugs} little bugs in the code`)
+    }
+
+})
+
+
+app.get("/pokemon", (req , res) => {
+    res.send(pokemon)
+})
+
+
+
+app.get("/pokemon/search", (req , res) => {
+    const { name } = req.query
+
+for( let i = 0; i < pokemon.length; i++){
+    if(name.toLowerCase() === pokemon[i].name.toLowerCase()){
+          res.send([pokemon[i]])
+          }   
+       
+        }
+    
+})
+
+
+app.get("/pokemon/:index", (req , res) => {
+    const {index} = req.params
+    if(pokemon[index]){
+        res.send(pokemon[index])
+    }
+    else{
+        res.send(`Sorry, no pokemon found at ${index}`)
+    }
+})
+
+
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,11 @@
+//Dependcies
+const app = require("./app.js")
+
+//Configuration
+require("dotenv").config()
+
+const PORT = process.env.PORT
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
+})
