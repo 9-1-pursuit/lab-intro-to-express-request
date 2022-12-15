a1097e5b1f5430ac7d40051520253bd509eacad6
MarsiaRochelin <marsrochelin@pursuit.org>
2022-12-15 06:48:06 -0500

    fixed the relative path for app

--- app.js
+++ app.js
@@ -3,3 +3,8 @@ const express = require("express");
 const app = express();
 
 //ROUTES
+app.get("/attentive/overwhelmed/developer", (req, res) => {
+  res.send(
+    "Congratulations on starting a new project called attentive-overwhelemd-developer!"
+  );
+});
