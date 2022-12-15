4a18b1a3e5f97e4ee3fa3997650e34bea713a29c
MarsiaRochelin <marsrochelin@pursuit.org>
2022-12-15 06:59:10 -0500

    updated .gitignore with node_modules

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
+//IMPORTS
 const app = require("./app.js");
 
 require("dotenv").config();
 
+//VARABLE ASSIGNMENTS
 const PORT = process.env.PORT;
 
+//LISTENING
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
