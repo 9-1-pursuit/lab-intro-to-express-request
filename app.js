b4e8258853b272ba94abfe371f9d92722ac4979d
Daniel-Mazzilli <danmazzilli@pursuit.org>
2022-12-15 09:09:30 -0500

    all passing. Tests and instructions have inconsistencies

--- app.js
+++ app.js
@@ -12,6 +12,31 @@ app.use(morgan("tiny"));
 
 // Routes
 
+app.get("/", (req, res) => {
+  res.status(200).send("Welcome 99 Pokemon");
+});
+
+app.get("/pokemon/search", (req, res) => {
+  const { name } = req.query;
+  const match = pokemon.filter(
+    (el) => el.name.toLocaleLowerCase() === name.toLowerCase()
+  );
+  res.status(200).send(match);
+});
+
+app.get("/pokemon/:indexOfArray", (req, res) => {
+  const { indexOfArray } = req.params;
+  if (pokemon[indexOfArray]) {
+    res.status(200).send(pokemon[indexOfArray]);
+  } else {
+    res.send(`Sorry, no pokemon found at ${indexOfArray}`);
+  }
+});
+
+app.get("/pokemon", (req, res) => {
+  res.status(200).send(pokemon);
+});
+
 app.get("/bugs/:numberOfBugs", (req, res) => {
   const { numberOfBugs } = req.params;
   if (numberOfBugs < 200) {
@@ -20,7 +45,7 @@ app.get("/bugs/:numberOfBugs", (req, res) => {
         <a href=/bugs/${+numberOfBugs + 2}>Pull one down, patch it around</a>`
     );
   } else {
-    res.status(200).send("<a href=/bugs>Too many bugs!! Start over!</a>");
+    res.send("<a href=/bugs>Too many bugs!! Start over!</a>");
   }
 });
 
