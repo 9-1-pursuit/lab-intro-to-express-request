4422acce9894edcb864c4856b3bc1752d4e5fd5a
MarsiaRochelin <marsrochelin@pursuit.org>
2022-12-15 07:41:28 -0500

    link added path in url when clicked

--- app.js
+++ app.js
@@ -5,12 +5,14 @@ const app = express();
 //ROUTES
 app.get("/attentive/overwhelmed/developer", (req, res) => {
   res.send(
-    "Congratulations on starting a new project called attentive-overwhelemd-developer!"
+    "Congratulations on starting a new project called attentive-overwhelemd-developer"
   );
 });
 
 app.get("/bugs", (req, res) => {
-  res.send("99 little bugs in the code");
+  res.send(
+    "99 little bugs in the code <a href=/bugs/101 >pull one down, patch it around </a>"
+  );
 });
 
 module.exports = app;
