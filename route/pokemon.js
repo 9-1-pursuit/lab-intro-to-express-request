0393d8ccb2a119eb3f620103f7db5af99a5b04ae
DestinyJoyner <destinyjoyner@pursuit.org>
2022-12-14 22:09:00 -0500

    moved bugs to route file

--- route/pokemon.js
+++ route/pokemon.js
@@ -11,6 +11,7 @@ router.get("/search", (req, resp) => {
     const input = req.query.name.toLowerCase()
     const thisPokemon = data.filter(({name}) => input === name.toLowerCase())
     const display = thisPokemon ? thisPokemon : `sorry we failed to 'Catch 'em All! `
+    
     resp.send(display)
 })
 
@@ -18,23 +19,8 @@ router.get("/:indexOfArray", (req, resp) => {
     const {indexOfArray} = req.params
     const thisPokemon = data[indexOfArray]
     const display = thisPokemon ? thisPokemon : `Sorry, no pokemon found at ${indexOfArray}`
+    
     resp.send(display)
-
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 module.exports = router
