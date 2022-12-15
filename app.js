877ae0d8b087602ad6c10005a6bbbff2a00e9641
MarsiaRochelin <marsrochelin@pursuit.org>
2022-12-15 08:51:24 -0500

    Test 1/3 passing

--- app.js
+++ app.js
@@ -3,16 +3,19 @@ const express = require("express");
 const app = express();
 
 //ROUTES
+app.get("/", (req, res) => {
+  res.send("Welcome 99 Pokemon");
+});
 app.get("/attentive/overwhelmed/developer", (req, res) => {
   res.send(
     "Congratulations on starting a new project called attentive-overwhelemd-developer"
   );
 });
-
 app.get("/bugs/:numberOfBugs", (req, res) => {
   const { numberOfBugs } = req.params;
   console.log(numberOfBugs);
-  if (numberOfBugs > 200) res.send("<a href=/bugs>start over</a>");
+  if (numberOfBugs >= 200)
+    res.send("Too many bugs!! Start over!<a href=/bugs>start over</a>");
   else
     res.send(
       "101 little bugs in the code<a href=/bugs/103 >pull one down, patch it around </a>"
