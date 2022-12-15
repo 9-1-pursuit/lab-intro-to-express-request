95cb855990a6857ef4291de06043ead04ecaf97e
Daniel-Mazzilli <danmazzilli@pursuit.org>
2022-12-15 08:45:35 -0500

    bugs section passing. I have some issues with the instructions and tests

--- app.js
+++ app.js
@@ -2,12 +2,37 @@
 const express = require("express");
 const morgan = require("morgan");
 
+const pokemon = require("./models/pokemon.json");
+// console.log(pokemon[0]);
+
 const app = express();
 
 // Middleware, see HTTP requests
 app.use(morgan("tiny"));
 
 // Routes
+
+app.get("/bugs/:numberOfBugs", (req, res) => {
+  const { numberOfBugs } = req.params;
+  if (numberOfBugs < 200) {
+    res.status(200).send(
+      `<h1>${numberOfBugs} little bugs in the code</h1>
+        <a href=/bugs/${+numberOfBugs + 2}>Pull one down, patch it around</a>`
+    );
+  } else {
+    res.status(200).send("<a href=/bugs>Too many bugs!! Start over!</a>");
+  }
+});
+
+app.get("/bugs", (req, res) => {
+  console.log(req.url);
+  res
+    .status(200)
+    .send(
+      "<h1>99 little bugs in the code</h1><a href=/bugs/101>Pull one down, patch it around</a>"
+    );
+});
+
 app.get("/:verb/:adjective/:noun", (req, res) => {
   const { verb, adjective, noun } = req.params;
   res
