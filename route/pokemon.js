23d88da8a8eb62c89b119ad4e13740857a8cf852
DestinyJoyner <destinyjoyner@pursuit.org>
2022-12-14 21:02:48 -0500

    created pokemon home route and index success

--- route/pokemon.js
+++ route/pokemon.js
@@ -3,7 +3,18 @@ let router = express.Router()
 
 const data = require("../models/pokemon.json")
 
-console.log(data[0])
+router.get("/", (req, resp) => {
+    const list = data.map(({name}) => name)
+    resp.send(list)
+})
+
+router.get("/:indexOfArray", (req, resp) => {
+    const {indexOfArray} = req.params
+    const thisPokemon = data[indexOfArray]
+    const display = thisPokemon ? thisPokemon : `sorry, no pokemon found at /pokemon/${indexOfArray}`
+    resp.send(display)
+
+})
 
 
 
