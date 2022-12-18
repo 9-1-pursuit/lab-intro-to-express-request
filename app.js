b867654b4d94abcd4f3d7fd77978d224b0fa5b33
bowersjuan <bowersjuan@gmail.com>
2022-12-18 15:17:00 -0500

    Add functionality for bugs routes and import models/pokemon.json

--- app.js
+++ app.js
@@ -1,17 +1,38 @@
 const express = require("express");
+const pokemon = require("./models/pokemon.json");
 
 const app = express();
 
-const bugsResponse = `<p>99 little bugs in the code</p>
-    <a href="" target="blank">
-      pull one down, patch it around
-    </a>`;
 app.get("/", (req, res) => {
   res.send("Welcome 99 Pokemon");
 });
 
 app.get("/bugs", (req, res) => {
-  res.send(bugsResponse);
+  const bugs = 99;
+  const data = `<p>${bugs} little bugs in the code</p>
+    <a href="http://localhost:8888/bugs/${Number(bugs) + Number(2)}">
+      pull one down, patch it around
+    </a>`;
+  res.send(data);
+});
+
+app.get("/bugs/:numberOfBugs", (req, res) => {
+  const { numberOfBugs } = req.params;
+  let data = "";
+
+  if (numberOfBugs > 200) {
+    data = `<p>${numberOfBugs} little bugs in the code</p>
+      <a href="http://localhost:8888/bugs">
+        start over
+      </a>`;
+  } else {
+    data = `<p>${numberOfBugs} little bugs in the code</p>
+      <a href="http://localhost:8888/bugs/${Number(numberOfBugs) + Number(2)}">
+        pull one down, patch it around
+      </a>`;
+  }
+
+  res.send(data);
 });
 
 app.get("/:verb/:adj/:noun", (req, res) => {
@@ -22,5 +43,7 @@ app.get("/:verb/:adj/:noun", (req, res) => {
       `${verb}-${adj}-${noun}!`
   );
 });
+
+console.log(pokemon[0]);
 
 module.exports = app;
